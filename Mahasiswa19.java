69da60280fd2224e53621cc8e36f65f483ef16aa
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-03-15 08:27:00 +0700

    Mahasiswa19.java

--- Mahasiswa19.java
+++ Mahasiswa19.java
@@ -1,4 +1,3 @@
-// Mahasiswa.java
 public class Mahasiswa19 {
     String nama;
     String nim;
@@ -15,9 +14,9 @@ public class Mahasiswa19 {
         this.nilaiUAS = nilaiUAS;
     }
 
-    // Method menampilkan data mahasiswa
+    // Method menampilkan data mahasiswa dengan format tabel
     public void tampilkanData() {
-        System.out.println(nama + " | " + nim + " | " + tahunMasuk + " | UTS: " + nilaiUTS + " | UAS: " + nilaiUAS);
+        System.out.printf("%-15s | %-10s | %-10d | UTS: %-3d | UAS: %-3d\n", nama, nim, tahunMasuk, nilaiUTS, nilaiUAS);
     }
 
     // Method Divide and Conquer mencari nilai UTS tertinggi
@@ -52,5 +51,33 @@ public class Mahasiswa19 {
             total += mhs.nilaiUAS;
         }
         return (double) total / data.length;
+    }
+
+    // Method untuk menampilkan header tabel
+    public static void tampilkanHeader() {
+        System.out.println("-----------------------------------------------------------");
+        System.out.printf("%-15s | %-10s | %-10s | %-6s | %-6s\n", "Nama", "NIM", "Tahun Masuk", "UTS", "UAS");
+        System.out.println("-----------------------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Mahasiswa19[] mahasiswa = {
+            new Mahasiswa19("Andi", "12345", 2020, 80, 85),
+            new Mahasiswa19("Budi", "12346", 2021, 75, 90),
+            new Mahasiswa19("Citra", "12347", 2019, 85, 80),
+            new Mahasiswa19("Dewi", "12348", 2022, 70, 75)
+        };
+
+        // Menampilkan data mahasiswa dalam format tabel
+        tampilkanHeader();
+        for (Mahasiswa19 mhs : mahasiswa) {
+            mhs.tampilkanData();
+        }
+        System.out.println("-----------------------------------------------------------");
+
+        // Menampilkan hasil pencarian nilai tertinggi, terendah, dan rata-rata
+        System.out.println("Nilai UTS Tertinggi: " + cariMaxUTS(mahasiswa, 0, mahasiswa.length - 1));
+        System.out.println("Nilai UTS Terendah: " + cariMinUTS(mahasiswa, 0, mahasiswa.length - 1));
+        System.out.printf("Rata-rata Nilai UAS: %.2f\n", hitungRataRataUAS(mahasiswa));
     }
 }
